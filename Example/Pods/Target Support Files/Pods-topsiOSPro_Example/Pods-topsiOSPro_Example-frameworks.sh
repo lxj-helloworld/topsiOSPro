76096c29cef943c60c40e51ae5eded5c7bb1a509
李效晋 <lixiaojin@topscomm.com>
2019-05-27 11:49:10 +0800

    字符串工具类

--- Example/Pods/Target Support Files/Pods-topsiOSPro_Example/Pods-topsiOSPro_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-topsiOSPro_Example/Pods-topsiOSPro_Example-frameworks.sh
@@ -161,9 +161,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/QorumLogs/QorumLogs.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/topsiOSPro/topsiOSPro.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/QorumLogs/QorumLogs.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/topsiOSPro/topsiOSPro.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
---
+++ utils/StringUtils.swift
@@ -0,0 +1,8 @@
+//
+//  StringUtils.swift
+//  FBSnapshotTestCase
+//
+//  Created by topscommmac01_lixiaojin on 2019/5/27.
+//
+
+import Foundation
